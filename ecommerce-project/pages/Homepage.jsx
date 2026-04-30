5432b419e006fafd5707d63e39fd542e549d3f24
sharmabhavik10 <bhaviksharma941@gmail.com>
2026-04-30 12:53:00 +0530

    Fetch data from the backend

--- ecommerce-project/pages/Homepage.jsx
+++ ecommerce-project/pages/Homepage.jsx
@@ -3,6 +3,13 @@ import { products } from './products';
 import './Homepage.css'
 
 export function Homepage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce-Project</title>
